c54556ef484a76c600c4c0206704bc084bd17064
unknown <Administrator@KGIL464.kg.local>
2022-02-04 11:50:17 +0530

    initial commit

---
+++ sidehover/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { TooltipModule } from 'ng2-tooltip-directive';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'sidehover';
+}
